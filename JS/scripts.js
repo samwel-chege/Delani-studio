754890da60b27505af74f5951667027f3469e0d7
samwel-chege <samwelchegeh09@gmail.com>
2021-06-20 13:44:11 +0300

    add id on decriptions

---
+++ JS/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('#icon1').on('click', function(){
+        $('#description1').toggle();
+    });
+});
+$(document).ready(function() {
+    $('#icon2').on('click', function(){
+        $('#description2').toggle();
+    });
+});
+$(document).ready(function() {
+    $('#icon3').on('click', function(){
+        $('#description3').toggle();
+    });
+});
