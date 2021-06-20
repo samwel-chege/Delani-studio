3c25847460860ecf8b55aa5ad5ac4085e3252e2f
samwel-chege <samwelchegeh09@gmail.com>
2021-06-20 15:07:58 +0300

    edit toggle effect

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,15 +1,30 @@
 $(document).ready(function() {
     $('#icon1').on('click', function(){
-        $('#description1').toggle();
+        $('#icon1').hide(function(){
+            $('#description1').show();
+        });
     });
-});
-$(document).ready(function() {
+    $('#description1').on('click', function(){
+        $('#icon1').show();
+        $('#description1').hide();
+    });
     $('#icon2').on('click', function(){
-        $('#description2').toggle();
+        $('#icon2').hide(function(){
+            $('#description2').show();
+        });
     });
-});
-$(document).ready(function() {
+    $('#description2').on('click', function(){
+        $('#icon2').show();
+        $('#description2').hide();
+    });
     $('#icon3').on('click', function(){
-        $('#description3').toggle();
+        $('#icon3').hide(function(){
+            $('#description3').show();
+        });
+    });
+    $('#description3').on('click', function(){
+        $('#icon3').show();
+        $('#description3').hide();
     });
 });
+   
